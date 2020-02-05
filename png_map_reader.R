7999ff3a940e4827fea79211685b04732047ab11
Gustav Lundberg <guslu389@student.liu.se>
2020-02-05 12:35:46 +0100

    corrected dimensions so X = Easting and Y = Northing

---
+++ png_map_reader.R
@@ -0,0 +1,35 @@
+png_map_reader <- function(mapname){
+  # Reads a georeferenced png-file into a data.table of X,Y,R,G,B values
+  # where X and Y are UTM-coordinates
+  require(png)
+  require(data.table)
+  stopifnot(all(file.exists(paste0(mapname,c(".png", ".pgw")))))
+  
+  png_worldfile_to_transform_matrix <- function(mapname){
+    # Reads worldfile into transformation matrix 
+    # Source: https://en.wikipedia.org/wiki/World_file
+    return(t(matrix(as.numeric(readLines(paste0(mapname,".pgw"))), ncol = 3)))
+  }
+  
+  png_map_to_df <- function(mapname){
+    # Reads 4-channel png-map into data.table and drops alpha-channel
+    map_dt <- dcast(as.data.table(readPNG(paste0(mapname,".png"))), 
+                    V1 + V2 ~ V3, value.var = "value")
+    setnames(map_dt, names(map_dt), c("pY", "pX", "R", "G", "B", "A"))
+    map_dt[, A := NULL]
+    return(map_dt)
+  }
+  
+  map_trns <- png_worldfile_to_transform_matrix(mapname)
+  map_dt   <- png_map_to_df(mapname)
+  
+  # Does the affine transform from pixels to coordinates
+  map_xy   <- data.table(as.matrix(cbind(map_dt[, .(pY,pX)], 1)) %*% map_trns)
+  
+  # Adds X and Y to map data and removes pixelcoordinates
+  map_dt[, c("Y", "X") := map_xy][, c("pX", "pY") := NULL]
+  setcolorder(map_dt, c("X", "Y", "R", "G", "B"))
+  
+  return(map_dt)
+}
+
