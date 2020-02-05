21032f2e984fda15c3321f06db9404fd10a9e61c
Gustav Lundberg <guslu389@student.liu.se>
2020-02-05 17:59:12 +0100

    Functionalized the lookup and made function factories for it. Also moved the lookup types into this file to have the relevant functions collected.

---
+++ dt_segment_lookup.R
@@ -0,0 +1,67 @@
+dt_closest <- function(xy, target_data, target_tol = 1){
+  # Retrieves RGB-values from target data for point in target data closest to given 
+  # coordinates (implicit from source data).
+  
+  # Subset all points within distance target_tol from x,y
+  closepts <- as.matrix(target_data[X>xy[1]-target_tol &
+                                      X<xy[1]+target_tol & 
+                                      Y>xy[2]-target_tol & 
+                                      Y<xy[2]+target_tol, 
+                                    .(X,Y,R,G,B)])
+  # Calculate distance matrix for these points and get index of closest one
+  closest <- which.min(dist(rbind(xy, closepts[,1:2]))[1:nrow(closepts)])
+  # Return original coord along with merged data
+  c(xy,closepts[closest,3:5])
+}
+
+dt_sq_average <- function(xy, target_data, target_tol = 1){
+  # Retrieves average RGB-values from points in target data within target_tol distance
+  # from given coordinates (implicit from source data).
+  # Subset all points within distance target_tol from x,y
+  closepts <- as.matrix(target_data[X>xy[1]-target_tol &
+                                      X<xy[1]+target_tol & 
+                                      Y>xy[2]-target_tol & 
+                                      Y<xy[2]+target_tol, 
+                                    .(X,Y,R,G,B)])
+  
+  # Return original coord along with merged data
+  c(xy,colMeans(closepts[closest,3:5]))
+}
+
+
+seg_lookup_factory <- function(seg_source, seg_target, target_tol, fun){
+  # Creates a function that runs dt_closest in the curent segment of data
+  function(point_no){
+    fun(xy = unlist(seg_source[point_no, .(X,Y)]), 
+        target_data = seg_target, 
+        target_tol = target_tol)
+  }
+}
+
+dt_lookup_factory <- function(map_grid, source_data, target_data, target_tol, fun){
+  # Creates a function subsets data for a segment and then runs closest lookup in those 
+  # data. For use in lapply.
+  function(seg_no){
+    write(paste("Segment", seg_no, "started at", Sys.time()), stdout())
+    seg_limits <- map_grid[seg_no, ]
+    
+    # Create  temporary data sets for source and target
+    seg_source <- source_data[X>=seg_limits$xmin & 
+                                X<=seg_limits$xmax & 
+                                Y>=seg_limits$ymin & 
+                                Y<=seg_limits$ymax,
+                              .(X,Y)]
+    seg_target <- target_data[X>=seg_limits$xmin-sfm_tol &
+                                X<=seg_limits$xmax+sfm_tol & 
+                                Y>=seg_limits$ymin-sfm_tol & 
+                                Y<=seg_limits$ymax+sfm_tol, 
+                              .(X,Y,R,G,B)]
+    
+    seg_lookup <- seg_lookup_factory(seg_source, seg_target, target_tol, fun)
+    
+    # Make cluster to speed up searching for values
+    cl <- makeForkCluster(6)
+    looked_up <-parSapply(cl = cl, X = seq.int(1, nrow(seg_source)), FUN = seg_lookup)
+    return(looked_up)
+  }
+}
