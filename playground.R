208dbe6be9b92786223a8ad5428a2265ea0c6ae8
napoleongl <guslu389@student.liu.se>
2020-02-06 21:45:18 +0100

    adapted to joining the whole datasets tidied up printouts somewhat

--- playground.R
+++ playground.R
@@ -36,19 +36,26 @@ sfm        <- las_reader(map_dir, map_grid, "sfm", sfm_tol)
 write(paste(nrow(las@data),"points to be looked up using", nrow(map_grid), "segments" ), stdout())
 # Process laslookup in sfm chunkwise
 cl <- parallel::makeForkCluster(floor(parallel::detectCores()/2))
-las_sfm_lookup <- dt_lookup_factory(map_grid, las@data, sfm@data, sfm_tol, dt_closest, cl)
+las_sfm_lookup <- dt_lookup_factory(map_grid = map_grid, 
+                                    source_data = las@data,
+                                    source_var = c("X","Y","Z","Intensity"), 
+                                    target_data = sfm@data, 
+                                    target_var = c("X", "Y", "R", "G", "B"), 
+                                    target_tol = sfm_tol, fun = dt_closest, cl = cl)
 
 #las_lookup  <- lapply(X = seq.int(1,nrow(map_grid)), FUN = las_sfm_lookup)
-las_lookup <- rbindlist(lapply(X = seq.int(1,5), FUN = las_sfm_lookup))
-setnames(las_lookup, names(las_lookup), c("X", "Y", "R", "G", "B"))
+las_lookup <- rbindlist(lapply(X = seq.int(1,4), FUN = las_sfm_lookup))
+setnames(las_lookup, names(las_lookup), c("X", "Y", "Z", "Intensity", "R", "G", "B"))
 
 save(las_lookup, file = paste0(map_dir, "/las_lookup.Rdata"))
 
 end_time <- Sys.time()
-total_time <- difftime(end_time, init_time, "mins")
-record_time <- difftime(end_time, init_time, "secs")/(nrow(las@data)/1000)
-write(paste("Result saved to disk.\nTotal time:", round(total_time,2), 
-             "minutes.\nTime per 1000 points:", round(record_time,3), "seconds" ), 
+total_time <- difftime(end_time, init_time, units = "mins")
+record_time <- difftime(end_time, init_time, units = "secs") / (nrow(las_lookup)/1000)
+time_record <- nrow(las_lookup) / as.numeric(difftime(end_time, init_time, units = "secs")) * 60
+write(paste("Result saved to disk.\nTotal time:", round(total_time, 2), 
+            "minutes.\nTime per 1K points:", round(record_time, 4), 
+            "seconds\nRecords per minute:", round(time_record, 1)), 
       stdout())
 stopCluster(cl)
 #load( paste0(map_dir, "/las_lookup.Rdata"))
