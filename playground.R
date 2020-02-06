a1f87cdf73f6d691223d2a2c52e521be6d013235
Gustav Lundberg <guslu389@student.liu.se>
2020-02-06 08:34:41 +0100

    moved library calls to functions and created function for handlign the las-files

--- playground.R
+++ playground.R
@@ -12,10 +12,8 @@
 
 source("png_map_reader.R")
 source("map_grid_maker.R")
-source("las_filter_string.R")
+source("las_reader.R")
 source("dt_segment_lookup.R")
-library(lidR)
-library(parallel)
 
 # Read Omap-png and create grid for lookup from that
 map_dir    <- "~/kartor/kvarn_liten"
@@ -23,23 +21,14 @@ map        <- png_map_reader(paste0(map_dir, "/omap_ren"))
 map_grid   <- map_grid_maker(map)
 
 # Read relevant LiDAR files
-las_tol     <- 0
-laspath    <- paste0(map_dir, "/laserdata/")
-lasfiles   <- paste0(laspath, dir(laspath, ".la[sz]+$"))
-las_select <- las_filter_string(map_grid, tol = las_tol) 
-las        <- readLAS(lasfiles, select = "xyzi", filter = las_select)
-setkey(las@data, X, Y)
+las_tol    <- 0
+las        <- las_reader(map_dir, map_grid, "las", las_tol)
 
 # Read relevant surfance model files
 sfm_tol    <- 1
-sfmpath    <- paste0(map_dir, "/ytmodell/")
-sfmfiles   <- paste0(sfmpath, dir(sfmpath, ".la[sz]+$"))
-sfm_select <- las_filter_string(map_grid, tol = sfm_tol) 
-sfm        <- readLAS(sfmfiles, select = "xyzRGB", filter = sfm_select)
-setkey(sfm@data, X, Y)
+sfm        <- las_reader(map_dir, map_grid, "sfm", sfm_tol)
 
 # Process laslookup in sfm chunkwise
-
 las_sfm_lookup <- dt_lookup_factory(map_grid, las@data, sfm@data, sfm_tol, dt_closest)
 
 #lapply(X = seq.int(1,nrow(map_grid)), FUN = function(seg_no){
