e1db4b5d9b4bfd88a30c1cb52445270a56e5269b
Gustav Lundberg <guslu389@student.liu.se>
2020-02-13 09:33:36 +0100

    restored colouring order

--- omap_colour_codes.R
+++ omap_colour_codes.R
@@ -1,29 +1,26 @@
 create_true_labels <- function(){
-  # Function for creating the set of true laels and their colours
+  # Create DT of colour codes and export those that are named
   suppressPackageStartupMessages(require(data.table))
-  .colour_matrix <- matrix(ncol = 4, byrow = TRUE,data = c(
-    1.00,1.00,1.00,"forest",
-    1.00,0.73,0.21,"open_ground",
-    1.00,0.87,0.60,"dense_forest", #doublecheck!
-    0.50,0.50,0.50,"building",
-    1.00,0.00,1.00,"marsh",
-    0.13,0.82,1.00,"water",
-    0.24,1.00,0.09,"very_dense_forest", #doublecheck!
-    0.77,1.00,0.73,"easy_forest", #doublecheck!
-    0.91,0.65,0.45,"trail",
-    0.00,0.00,0.00,"road"
-  ))
+  col_codes <- matrix(ncol = 3, byrow=TRUE, dimnames = list(c(), c("R", "G", "B")), c(
+    000,  000,  000, 
+    100,  240,  255, 
+    020,  120,  200, 
+    150,  255,  138, 
+    051,  160,  044, 
+    251,  050,  153,  
+    227,  026,  040, 
+    253,  191,  111, 
+    255,  127,  000, 
+    220,  150,  230, 
+    106,  061,  154, 
+    255,  255,  255  
+  ))/255
+  ccodes <- data.table(col_codes)[, colour := rgb(R,G,B)]
+  ccodes[, category := c("roads", "water", "marsh", "openground", 
+                         "denseforest", "building", "trail", "unused", 
+                         "mediumforest", "unused", "mountains" , "forest")]
   
-  
-  true_labels <- data.table(R = as.numeric(.colour_matrix[,1]),
-                            G = as.numeric(.colour_matrix[,2]),
-                            B = as.numeric(.colour_matrix[,3]),
-                            category = .colour_matrix[,4])
-  
-  true_labels[, c("colour", "id") := list(rgb(true_labels[, .(R,G,B)]), 1:.N)]
-  setkey(true_labels, colour)
-  write.csv(true_labels[, .(category, id)], "true_labels.csv", row.names = FALSE)
-  return(true_labels)
+  ccodes[category!="unused", ][, ID:= 1:.N]
 }
 
 .plot_categories <- function(true_labels){
@@ -35,32 +32,14 @@ create_true_labels <- function(){
     theme_void()
 }
 
-col_codes <- matrix(ncol = 3, byrow=TRUE, c(
-  000,  000,  000, #road
-  100,  240,  255, #water
-  020,  120,  200, #marsh
-  150,  255,  138, #openground
-  051,  160,  044, #denseforest
-  251,  050,  153, #building  
-  227,  026,  040, #trail
- #253,  191,  111, #unused
-  255,  127,  000, #mediumforest
-  220,  150,  230, #unused
-  106,  061,  154, #mountain
-  255,  255,  255  #lightforest
-  ))
-colnames(col_codes) <- c("R", "G", "B")
 
-round(as.matrix(dist(col_codes)))
-min(dist(col_codes))
+#round(as.matrix(dist(col_codes)))
+#min(dist(col_codes))
+# plotly::plot_ly(x=ccodes[,R], y=ccodes[,G], z=ccodes[,B], color=ccodes[,ID], mode="marker")
+# 
+# library(ggplot2)
+# ggplot(ccodes, aes(x=ID, fill=category,y=0,height=1,width=1)) +
+#   geom_tile(col="grey50") +
+#   scale_fill_manual(values=setNames(ccodes$HEX,ccodes$category)) + 
+#   theme_void()
 
-ccodes <- data.table(col_codes)[, ID := 1:.N][, HEX := rgb(R,G,B, maxColorValue = 255)]
-plotly::plot_ly(x=ccodes[,R], y=ccodes[,G], z=ccodes[,B], color=ccodes[,ID], mode="marker")
-
-ccodes[, category := c("roads", "water","marsh", "opneground","denseforest","building","trail","unused1","mediumforest","unused2","mountain", "lightforest")]
-
-library(ggplot2)
-ggplot(ccodes, aes(x=ID, fill=category,y=0,height=1,width=1)) +
-  geom_tile(col="grey50") +
-  scale_fill_manual(values=setNames(ccodes$HEX,ccodes$category)) + 
-  theme_void()
