3075dedd733ec930d07f92a9e520791880d66dc0
Gustav Lundberg <guslu389@student.liu.se>
2020-02-14 13:46:26 +0100

    adapted for sequential files

--- las_data_prep.R
+++ las_data_prep.R
@@ -1,103 +1,118 @@
-args <- commandArgs(TRUE)
+suppressPackageStartupMessages(library("optparse"))
 
-if(length(args)==0){
-  print("No arguments supplied. Using defaults!")
-  # Default values
-  map_dir    <- "~"
-  seg_size   <- 125
-  runmode    <- 4
-  output_dir <- paste0(getwd(), "/area_output/")
-} else {
-  map_dir    <- args[[1]]
-  seg_size   <- as.numeric(args[[2]])
-  runmode    <- args[[3]]
-  output_dir <- args[[4]]
-}
+option_list <- list( 
+  # make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
+  #             help="Print extra output [default]"),
+  # make_option(c("-q", "--quietly"), action="store_false", 
+  #             dest="verbose", help="Print little output"),
+  make_option(c("-m", "--source_dir"), type="character", default="~/kartor/", 
+              help="Directory where source maps are located as subfolders [default %default]",
+              dest = "source_dir"),
+  make_option(c("-o", "--output_dir"), type = "character", default = paste0(getwd(), "/area_output/"),
+              help = "Directory where to output treated files. [default %default]",
+              dest = "output_dir"),
+  make_option(c("-s", "--seg_size"), type = "integer", default = 100,
+              help = "Size of chunks to be processed and represent sub area [default %default]",
+              dest = "seg_size"),
+  make_option(c("-r", "--runmode"), type = "character", default = "4",
+              help = "Maximum number of segments to run. Non-integer to run all [default %default]",
+              dest = "runmode")
+  )
 
-init_time_0 <- Sys.time()
-write(paste("Started at", init_time_0),stdout())
+opt_parser <- OptionParser(option_list=option_list);
+opts       <- parse_args(opt_parser);
+
 source("png_map_reader.R")
 source("map_grid_maker.R")
 source("las_reader.R")
 source("dt_segment_lookup.R")
 source("omap_colour_codes.R")
 source("little_helpers.R")
-#
-map_dir    <- "~/LIU/kartor/"
-seg_size   <- 125
-runmode    <- 4
-output_dir <- paste0(getwd(), "/dataprep/")
+
+init_time_0<- Sys.time()
+source_dir <- opts$source_dir
+output_dir <- opts$output_dir
+seg_size   <- opts$seg_size
+runmode    <- opts$runmode
 
 # Create set of true labels
 true_labels<- create_true_labels()
 
-# Ensure output directories exists
-areas      <- dir(map_dir)
-areas      <- areas[!areas %in% c("laserdata", "ytmodell")]
+# Traverse input dir and ensure output directories exists
+areas      <- dir(source_dir)
+areas      <- areas[!areas %in% c("_laserdata", "_ytmodell", "_other")]
+write(paste("Started at", init_time_0,"\nFound ", length(areas), "areas to process."),
+      stdout())
 if(!dir.exists(output_dir)) {dir.create(output_dir)}
 for(area in areas){
+  area_init   <- Sys.time()
+  write(paste0("Starting area ", area, " at ", area_init), stdout())
   curr_output <- paste0(output_dir, area, "/")
-  curr_input  <- paste0(map_dir, area, "/")
+  curr_source <- paste0(source_dir, area, "/")
   if(!dir.exists(curr_output)) {dir.create(curr_output)}
+  
+  # Read Omap-png and create grid for lookup from that
+  mapname    <- dir(curr_source,".png$")
+  omap       <- png_map_reader(mapfile = paste0(curr_source, mapname), true_categories = true_labels)
+  omap_grid  <- map_grid_maker(omap, seg_size = seg_size)
+  
+  # Read relevant LiDAR files
+  las_tol    <- 0
+  las        <- las_reader(source_dir, omap_grid, "las", las_tol)
+  
+  # Read relevant surfance model files
+  sfm_tol    <- 1
+  sfm        <- las_reader(source_dir, omap_grid, "sfm", sfm_tol)
+  
+  write(paste0("Prework done in ", 
+               round(difftime(Sys.time(), area_init, units = "secs"),1), " s."),
+        stdout())
+  
+  # To allow for running test mode with fewer segments, try and cast runmode as integer
+  # If possible only run that many segments, else run all.
+  end_seg    <- suppressWarnings(ifelse(is.na(as.integer(runmode)), 
+                                        nrow(omap_grid),
+                                        min(as.integer(runmode), nrow(omap_grid))))
+  
+  #write(paste(nrow(las@data),"points to be looked up using", end_seg, "segments" ), stdout())
+  
+  # Process laslookup in sfm chunkwise
+  init_time_1 <- Sys.time()
+  cl         <- parallel::makeForkCluster(floor(parallel::detectCores()/2))
+  las_sfm_lookup <- dt_lookup_factory(map_grid = omap_grid, 
+                                      by = c("X", "Y"),
+                                      source_data = las@data,
+                                      source_var = c("Z","Intensity"), 
+                                      target_data = sfm@data, 
+                                      target_var = c("R", "G", "B"), 
+                                      target_tol = sfm_tol, fun = dt_closest, cl = cl)
+  
+  las_sfm_join <- rbindlist(lapply(X = seq.int(1, end_seg), FUN = las_sfm_lookup))
+  stopCluster(cl)
+  timing_writer(init_time_1, Sys.time(), nrow(las_sfm_join))
+  
+  # Process las-lookup in omap
+  init_time_2 <- Sys.time()
+  cl         <- parallel::makeForkCluster(floor(parallel::detectCores()/2))
+  las_omap_lookup <- dt_lookup_factory(map_grid = omap_grid, 
+                                       by = c("X", "Y"),
+                                       source_data = las_sfm_join,
+                                       source_var = c("Z","Intensity", "R", "G", "B"), 
+                                       target_data = omap, 
+                                       target_var = c("category"), 
+                                       target_tol = 1, fun = dt_closest, cl = cl)
+  
+  #las_omap_join <- rbindlist(lapply(X = seq.int(1,end_seg), FUN = las_omap_lookup))
+  las_omap_join <- lapply(X = seq.int(1,end_seg), FUN = las_omap_lookup)
+  stopCluster(cl)
+  # save(las_omap_join, file = paste0(source_dir, "/las_lookup_2.Rdata"))
+  #timing_writer(init_time_2, Sys.time(), nrow(las_sfm_join))
+  
+  seg_writer <- seg_list_writer_factory(output_dir = curr_output, 
+                                        data_list = las_omap_join)
+  invisible(lapply(1:length(x = seq.int(1,end_seg)), seg_writer))
+  timing_writer(init_time_2, Sys.time(), nrow(las_sfm_join))
 }
-
-# Read Omap-png and create grid for lookup from that
-mapname    <- dir(curr_input,".png$")
-omap       <- png_map_reader(paste0(curr_input, mapname), true_labels)
-omap_grid  <- map_grid_maker(omap, seg_size = seg_size)
-
-# Read relevant LiDAR files
-las_tol    <- 0
-las        <- las_reader(map_dir, omap_grid, "las", las_tol)
-
-# Read relevant surfance model files
-sfm_tol    <- 1
-sfm        <- las_reader(map_dir, omap_grid, "sfm", sfm_tol)
-
-end_time_0 <- Sys.time()
-write(paste0("Prework done in ", 
-             round(difftime(end_time_0, init_time_0, units = "secs"),1), " s."),
+write(paste0(length(areas), " areas finished in ", 
+             round(difftime(Sys.time(), init_time_0, units = "mins"),1), " min."),
       stdout())
-
-# To allow for running test mode with fewer segments, try and cast runmode as integer
-# If possible only run that many segments, else run all.
-end_seg    <- suppressWarnings(ifelse(is.na(as.integer(runmode)), 
-                                      nrow(omap_grid),
-                                      min(as.integer(runmode), nrow(omap_grid))))
-
-write(paste(nrow(las@data),"points to be looked up using", end_seg, "segments" ), stdout())
-
-# Process laslookup in sfm chunkwise
-init_time_1 <- Sys.time()
-cl         <- parallel::makeForkCluster(floor(parallel::detectCores()/2))
-las_sfm_lookup <- dt_lookup_factory(map_grid = omap_grid, 
-                                    by = c("X", "Y"),
-                                    source_data = las@data,
-                                    source_var = c("Z","Intensity"), 
-                                    target_data = sfm@data, 
-                                    target_var = c("R", "G", "B"), 
-                                    target_tol = sfm_tol, fun = dt_closest, cl = cl)
-
-las_sfm_join <- rbindlist(lapply(X = seq.int(1, end_seg), FUN = las_sfm_lookup))
-stopCluster(cl)
-timing_writer(init_time_1, Sys.time(), nrow(las_sfm_join))
-
-# Process las-lookup in omap
-init_time_2 <- Sys.time()
-cl         <- parallel::makeForkCluster(floor(parallel::detectCores()/2))
-las_omap_lookup <- dt_lookup_factory(map_grid = omap_grid, 
-                                     by = c("X", "Y"),
-                                     source_data = las_sfm_join,
-                                     source_var = c("Z","Intensity", "R", "G", "B"), 
-                                     target_data = omap, 
-                                     target_var = c("cat_id"), 
-                                     target_tol = 1, fun = dt_closest, cl = cl)
-
-#las_omap_join <- rbindlist(lapply(X = seq.int(1,end_seg), FUN = las_omap_lookup))
-las_omap_join <- lapply(X = seq.int(1,end_seg), FUN = las_omap_lookup)
-stopCluster(cl)
-save(las_omap_join, file = paste0(map_dir, "/las_lookup_2.Rdata"))
-timing_writer(init_time_2, Sys.time(), nrow(las_sfm_join))
-
-seg_writer <- seg_list_writer_factory(output_dir, las_omap_join)
-invisible(lapply(1:length(x = seq.int(1,end_seg)), seg_writer))
