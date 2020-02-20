ed69e25f71f6a2d9d8f3283a2afcb25a2139f396
Gustav Lundberg <guslu389@student.liu.se>
2020-02-20 14:52:27 +0100

    minor changes to facilitate z-filtering

--- las_reader.R
+++ las_reader.R
@@ -1,30 +1,24 @@
-las_filter_string <- function(map_grid, tol=0){
+las_filter_string <- function(map_grid, tol=0, zlim ){
   # Function to build the string used in filter-arg of lidR::readLAS
   # given a the extents of a map
   xmin <- paste("-drop_x_below", min(map_grid$xmin)-tol)
   xmax <- paste("-drop_x_above", max(map_grid$xmax)+tol)
   ymin <- paste("-drop_y_below", min(map_grid$ymin)-tol)
   ymax <- paste("-drop_y_above", max(map_grid$ymax)+tol)
-  zmax <- paste("-drop_z_above 200")
-  paste(xmin, xmax, ymin, ymax, zmax ,sep = ", ")
+  zmin <- paste("-drop_z_below", zlim["min"])
+  zmax <- paste("-drop_z_above", zlim["max"])
+  paste(xmin, xmax, ymin, ymax, zmin, zmax ,sep = ", ")
 }
 
-las_reader <- function(map_cat = NULL, map_dir = NULL, map_grid, type, tol){
+las_reader <- function(map_cat = NULL, las_dir = NULL, map_grid, select_string, tol, zlim){
   # Function to read all .las/Z files in dir given type and filter
   # out only the relevant areas. Alternatively can read from a catalog.
   suppressPackageStartupMessages(require(lidR))
-  if(type == "las"){
-    path          <- paste0(map_dir, "_laserdata/")
-    select_string <- "xyzi"
-  } else if(type == "sfm"){
-    path          <- paste0(map_dir, "_ytmodell/")
-    select_string <- "xyzRGB"
-  }
-  files         <- paste0(path, dir(path, ".la[sz]+$"))
-  filter_string <- las_filter_string(map_grid, tol = tol) 
+  filter_string <- las_filter_string(map_grid, tol = tol, zlim) 
   if(!is.null(map_cat)){
     lasdata       <- readLAS(files = map_cat, select = select_string, filter = filter_string)
   } else {
+    files         <- paste0(las_dir, dir(las_dir, ".la[sz]+$"))
     lasdata       <- readLAS(files = files, select = select_string, filter = filter_string)
   }
   setkey(lasdata@data, X, Y) # To ensure indexing is done
