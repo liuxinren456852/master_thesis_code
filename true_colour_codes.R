d9ed9bfa065c32a5f9c8cea5601d3108f9ac8099
Gustav Lundberg <guslu389@student.liu.se>
2020-02-20 11:41:50 +0100

    removed mountains and added export to plotting

--- true_colour_codes.R
+++ true_colour_codes.R
@@ -18,16 +18,22 @@ create_true_labels <- function(){
   ccodes <- data.table(col_codes)[, colour := rgb(R,G,B)]
   ccodes[, category := c("roads", "water", "marsh", "openground", 
                          "denseforest", "building", "trail", "unused", 
-                         "mediumforest", "unused", "mountains" , "forest")]
+                         "mediumforest", "unused", "unusedmountains" , "forest")]
   
-  ccodes[category!="unused", ][, ID:= 1:.N]
+  ccodes[!grepl(x = ccodes$category, pattern = "unused"), ][, ID:= 0:(.N-1)]
 }
 
-.plot_categories <- function(true_labels){
+.plot_categories <- function(true_labels, export = FALSE){
   library(ggplot2)
-  ggplot(true_labels, aes(y=1:length(colour), x=0, width = 1, height = 1)) +
+  cat_plot <- ggplot(true_labels, aes(y=1:length(colour), x=0, width = 1, height = 1)) +
     geom_tile(fill = true_labels$colour, col = "black", size = rel(1)) +
     geom_label(aes(label = colour, family ="serif"), size = rel(3), nudge_x = -.45,hjust = "inward") +
-    geom_label(aes(label = category, family = "serif"), size = rel(3), nudge_x = .45, hjust="inward", ) +
+    geom_label(aes(label = paste0(ID, ": ", category), family = "serif"), size = rel(3), nudge_x = .45, hjust="inward", ) +
     theme_void()
+  if(export){ 
+      ggsave("omap_categories.pdf", cat_plot,width = 8, height = 2.5, units = "in") 
+    } else { 
+      return(cat_plot) 
+    }
 }
+
