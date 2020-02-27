e4b0095f008c721852dde74f9500ebf5abfe4001
Gustav Lundberg <guslu389@student.liu.se>
2020-02-27 11:43:36 +0100

    changed so to get proper categories are populated from true_labels

--- png_map_reader.R
+++ png_map_reader.R
@@ -33,7 +33,7 @@ png_map_reader <- function(mapfile, true_categories){
   # Adds X and Y to map data. Pixelcoords kept to enable plotting
   map_dt[, c("X", "Y") := map_xy][, colour := rgb(map_dt[,.(R,G,B)])]
   map_dt[, c("cat_colour") := true_categories[cat_dists, .(colour)]]
-  map_dt[, c("category") := cat_dists]
+  map_dt[, c("category") := cat_dists-1]
   setcolorder(map_dt, c("X", "Y", "category", "R", "G", "B", "colour", "cat_colour", "pX", "pY"))
   setkey(map_dt, X, Y)
   return(map_dt)
